3a981ff1a33a53620300780e4e6863f3e056b7a2
Meshabaz <shabazameen1818@gmail.com>
2022-12-02 19:30:43 +0530

    payment successful

--- script/checkout.js
+++ script/checkout.js
@@ -67,5 +67,72 @@ const applyPromo = () => {
   display();
   document.querySelector(".promoCode").value = null;
 };
-
 document.querySelector(".apply").addEventListener("click", applyPromo);
+
+const pay = document.querySelector(".pay");
+pay.addEventListener("click", () => {
+  document.querySelector(".drop").style.display = "flex";
+});
+
+document.querySelector(".close_p").addEventListener("click", () => {
+  document.querySelector(".drop").style.display = "none";
+});
+
+document.querySelector(".send_otp").addEventListener("click", () => {
+  alert("1234");
+  document.querySelector(".drop").style.display = "none";
+  document.querySelector(".dropon").style.display = "flex";
+});
+document.querySelector(".verify_otp").addEventListener("click", () => {
+  let otp = document.querySelector(".otp").value;
+  if (otp === "1234") {
+    alert("congratulation ! Order Placed.");
+    document.querySelector(".dropon").style.display = "none";
+    otp.value = null;
+  } else {
+    alert("OOP's ! You entred wrong otp, Please Try Again.");
+    otp.value = null;
+  }
+});
+
+////////////=======> APPLIYING THE POPS <=======/////////////
+
+// const pay = document.querySelector(".pay");
+// pay.addEventListener("click", () => {
+//   const model_body = document.querySelector(".modal-content");
+//   // model_body.innerHTML = "hi";
+//   // alert("hi");
+//   model_body.innerHTML = `
+//                 <div class="modal-header">
+//                     <h1 class="modal-title fs-5" id="staticBackdropLabel">Proceed To Payment</h1>
+//                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//                 </div>
+//                 <div class="modal-body phonepe">
+//                     <div class="my-4">
+//                         <h4><strong>
+//                                 Scan & Pay via PhonePe
+//                             </strong></h4>
+
+//                         <img src="assets/QR.jpeg" alt="">
+//                     </div>
+//                     <h4 class="or">OR</h4>
+//                     <div>
+//                         <h4><strong>
+//                                 Enter Your Mobile Number
+//                             </strong></h4>
+//                         <input type="number" placeholder="0 0 0 0 0 - 0 0 0 0 0" class="form-control my-4"
+//                             id="exampleFormControlInput1">
+//                         <button class="next send_otp btn btn-primary">Send OTP</button>
+//                     </div>
+//                 </div>
+//             `;
+// });
+
+// <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+//       aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+//       <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+//           <div class="modal-content">
+// </div>
+//       </div>
+//   </div>`;
