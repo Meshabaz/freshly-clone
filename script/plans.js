fb01f5eaeb15e09fd5ca9e354cd2db0dfe58d346
Meshabaz <shabazameen1818@gmail.com>
2022-12-02 12:18:10 +0530

    added funcs

--- script/plans.js
+++ script/plans.js
@@ -37,29 +37,29 @@ function dosomething(e) {
 
 // =============> MESSAGE CODE  <================= //
 
-parent.addEventListener("click", show_message, false);
+// parent.addEventListener("click", show_message, false);
 
-const toast = document.querySelector(".theToast"),
-  closeIcon = document.querySelector(".close"),
-  progress = document.querySelector(".progress");
+// const toast = document.querySelector(".theToast"),
+//   closeIcon = document.querySelector(".close"),
+//   progress = document.querySelector(".progress");
 
-function show_message(e) {
-  toast.classList.add("active");
-  progress.classList.add("active");
+// function show_message(e) {
+//   toast.classList.add("active");
+//   progress.classList.add("active");
 
-  setTimeout(() => {
-    toast.classList.remove("active");
-  }, 4000);
-  setTimeout(() => {
-    progress.classList.remove("active");
-  }, 4300);
-}
+//   setTimeout(() => {
+//     toast.classList.remove("active");
+//   }, 4000);
+//   setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 4300);
+// }
 
-closeIcon.addEventListener("click", () => {
-  toast.classList.remove("active");
-  progress.classList.add("active");
+// closeIcon.addEventListener("click", () => {
+//   toast.classList.remove("active");
+//   progress.classList.add("active");
 
-  setTimeout(() => {
-    progress.classList.remove("active");
-  }, 300);
-});
+//   setTimeout(() => {
+//     progress.classList.remove("active");
+//   }, 300);
+// });
